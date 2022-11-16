ef540e13681e4e972c2737d76dada331c310e77d
Pranav Borse <pranavborse91@gmail.com>
2022-11-16 17:25:25 +1100

    Create grid logic
    The code now creates a grid based on the value of the slider.

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const slider = document.querySelector('.slider');
 const value = document.querySelector('.value');
+
 value.textContent = `${slider.value} x ${slider.value}`;
+
 slider.oninput = function(){
     value.textContent = `${this.value} x ${this.value}`;
+    createBoard(this.value);
 }
-
 
 function createBoard(size){
-let board = document.querySelector('.board');
-let squares = board.querySelectorAll('div');
-squares.forEach((div) => div.remove());
-board.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
-let area = size * size
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
+    let area = size * size
 
-for(let i = 0; i < area; i++){
-    let square = document.createElement('div');
-    square.style.cssText = 'background: blue';
-    board.insertAdjacentElement('beforeend', square);
+    for(let i = 0; i < area; i++){
+        let square = document.createElement('div');
+        square.style.cssText = 'background: blue';
+        board.insertAdjacentElement('beforeend', square);
+    }
 }
-}
-
 createBoard(16);
